79b7c18b2ae12aa2a1714de692b9b86f77eaa9ec
GoG_Paulie <ppolih21@gmail.com>
2020-06-30 19:00:25 -0400

    Updates

---
+++ precache-manifest.106aaaca29e5bea327e11f18abc1241d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "811c643f651a4d03e45f5fbc36b8a447",
+    "url": "/gh-pages-react/index.html"
+  },
+  {
+    "revision": "189f7cfec8a12f329b3c",
+    "url": "/gh-pages-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "36fb853b09e8c9b74812",
+    "url": "/gh-pages-react/static/js/2.be7e122f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gh-pages-react/static/js/2.be7e122f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "189f7cfec8a12f329b3c",
+    "url": "/gh-pages-react/static/js/main.6b03205c.chunk.js"
+  },
+  {
+    "revision": "48e5b42b499bd35fa7ba",
+    "url": "/gh-pages-react/static/js/runtime-main.23de06ff.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gh-pages-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
