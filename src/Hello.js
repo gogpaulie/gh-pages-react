65b56ad044a548ff8fe20fae625b3752947e883a
GoG_Paulie <ppolih21@gmail.com>
2020-06-30 20:46:16 -0400

    adding ! to h1

--- src/Hello.js
+++ src/Hello.js
@@ -4,7 +4,7 @@ class Hello extends React.Component {
 	render() {
 		return (
 			<div className='f1 tc'>
-				<h1 className='bg-red'>Hello World</h1>
+				<h1 className='bg-red'>Hello World!</h1>
 				<p>{this.props.greeting}</p>
 			</div>
 		)
