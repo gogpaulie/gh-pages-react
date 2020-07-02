8812f083a0422e42568a80dfb51253ced25e3ee0
GoG_Paulie <ppolih21@gmail.com>
2020-07-01 21:44:44 -0400

    create robofriends app

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-import Hello from './Hello';
+import App from './Components/App';
 import 'tachyons';
+
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Hello greeting={"Hello React Ninja"}/>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
